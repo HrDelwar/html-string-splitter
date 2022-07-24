36fcdcbbad7ed13f1caab7c7fa4075f382531a70
Habibur Rahman Delwar <hrdelwar75@gmail.com>
2022-07-24 16:20:41 +0600

    publish-as-npm

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # html-string-splitter
-Split html string by length and word count
+Split html string by character or word count
+<hr>
+
+Simple installation by npm 
+```
+npm install html-string-splitter
+```
+
+
+###For commonjs
+
+
+```
+const splitter = require('html-string-splitter');
+
+console.log(
+    splitter.splitByCharacterCount(
+        `<p>This is text</p>split by <a href="#">character</a><strong>length </strong><p>also more text </p>`,
+         15
+    )
+)
+/*
+// output
+  <p>This is text</p>spl...<a href="#"></a><strong></strong><p></p>
+* */
+
+console.log(
+    splitter.splitByWordCount(
+        `<p>This is text</p>split by <a href="#">character</a><strong>length </strong><p>also more text </p>`,
+         6
+    )
+)
+/*
+// output
+<p>This is text</p>split by <a href="#">character...</a><strong></strong><p></p>
+*/
+```
+
+
+
+###For ES6 module
+```
+import {splitByCharacterCount, splitByWordCount} from 'html-string-splitter';
+
+console.log(
+    splitByCharacterCount(
+        `<p>This is text</p>split by <a href="#">character</a><strong>length </strong><p>also more text </p>`,
+         15
+     )
+)
+/*
+// output
+  <p>This is text</p>spl...<a href="#"></a><strong></strong><p></p>
+* */
+
+console.log(
+    splitByWordCount(
+        `<p>This is text</p>split by <a href="#">character</a><strong>length </strong><p>also more text </p>`,
+        6
+    )
+)
+/*
+// output
+<p>This is text</p>split by <a href="#">character...</a><strong></strong><p></p>
+*/
+```
+
+### With button
+* Note - you can pass html button on third parameter which will added last of count
+```
+console.log(
+    splitByCharacterCount(
+        `<p>This is text</p>split by <a href="#">character</a><strong>length </strong><p>also more text </p>`,
+         15,
+         <button>Read More</button>
+    )
+)
+/*
+// output
+  <p>This is text</p>spl...<btton>Read More</btton><a href="#"></a><strong></strong><p></p>
+* */
+
+```
---
+++ index.js
@@ -0,0 +1,61 @@
+// split html string with character count
+function htmlStringSplitter(html = '', count = 0, by = '', btn = ''){
+    if(!html && typeof html !== 'string') return null;
+    count = Number(count);
+    if(!count) {
+        return null;
+    }
+    let htmlArr = html.match(/<[^> ]+[^>]*>[^<]*/g)
+    let htmlStr = '',  totalCount = 0;
+
+    for ( let i = 0; i < htmlArr.length; i++){
+        let element = htmlArr[i], tag, str;
+        str = element.replace( /(<([^>]+)>)/ig, '').trim();
+        if (by === 'word') {
+            str =  str.split(' ').filter(el => el.length !== 0);
+        }
+
+        tag = element.match(/(<([^>]+)>)/ig)[0];
+        totalCount += str.length;
+
+        if (totalCount <= count) {
+            htmlStr += element
+            if (!str.length) {
+                continue;
+            }
+            if (totalCount === count) {
+                htmlStr += '...' + (btn && typeof btn === 'string' ? btn : '');
+            }
+        } else {
+            htmlStr += tag;
+            if (!str.length) {
+                continue;
+            }
+            totalCount -= str.length;
+            let restCount = count - totalCount;
+            if (restCount >= 1 && (totalCount + restCount) === count) {
+                if (typeof str === 'string') {
+                    htmlStr += str.substring(0, restCount);
+                }
+                if (typeof str === 'object') {
+                    htmlStr += str.slice(0, restCount).join(' ');
+                }
+                htmlStr += '...' + (btn && typeof btn === 'string' ? btn : '');
+            }
+            totalCount += str.length;
+        }
+
+    }
+    return htmlStr;
+}
+
+function splitByWordCount(html, count, btn) {
+    return htmlStringSplitter(html, count, 'word', btn);
+}
+ function splitByCharacterCount(html, count, btn) {
+    return htmlStringSplitter(html, count, '', btn);
+}
+module.exports ={
+    splitByCharacterCount,
+    splitByWordCount
+}
